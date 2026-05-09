b0e3e5d0f56c721d5f442c4a4c2c170ad1abac9f
NataliPerezG <nataperezg22@gmail.com>
2026-05-09 09:05:48 -0500

    understing hoisting and currying

--- src/js/scope.js
+++ src/js/scope.js
@@ -1,18 +1,31 @@
-function makeAdder(x) {
-    return function (y) {
-        return x + y
+
+function descuento(porcent) {
+    return precio => precio - (precio * porcent)
+}
+
+const diez = descuento(0.10)
+const quince = descuento(0.15)
+
+const desc1 = diez(500)
+const desc2 = quince(500)
+
+console.log(desc1);
+console.log(desc2);
+
+
+function msg(type) {
+    return (messagge) => {
+        let hour = new Date().toLocaleTimeString()
+        return `[${hour}]: ${type}: ${messagge}`
     }
 }
 
-let base = makeAdder(4);
-let add5 = base(5)
-let add10 = base(10)
+const error = msg('Error')
+const error1 = error('Datos incompletos')
+const success = msg('Éxito')
+const success1 = success('Registro guardado!')
 
-console.log(add5);
-console.log(add10);
-console.log(add10);
-console.log(add10);
-console.log(add5);
-
+console.log(error1);
+console.log(success1);
 
 
